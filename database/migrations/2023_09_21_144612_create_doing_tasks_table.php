770c9700203c785bbcc37506ea60600ac6707ed9
malackapite <veress.marcell@protonmail.com>
2023-09-21 16:53:43 +0200

    role + doing task tábla :cat2::dash:

---
+++ database/migrations/2023_09_21_144612_create_doing_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doing_tasks', function (Blueprint $table) {
+            $table->integer("amongus_id");
+            $table->integer("task_id");
+            $table->primary(["amongus_id","task_id"]);
+            $table->foreignId("amongus_id")->references("id")->on("amongus");
+            $table->foreignId("task_id")->references("id")->on("task");
+            $table->boolean("is_done");
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doing_tasks');
+    }
+};
