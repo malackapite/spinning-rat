01046f3d23e1cc3325020925a82b9531ef0408f7
KeresztesHunor <kereszteshunor36gmail.com>
2023-09-22 11:50:47 +0200

    amonguses adatok

--- database/migrations/2023_09_21_694200_create_amonguses_table.php
+++ database/migrations/2023_09_21_694200_create_amonguses_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Amongus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -18,6 +19,26 @@ return new class extends Migration
             $table->foreignId('role_id')->references('id')->on('roles');
             $table->timestamps();
         });
+        Amongus::create([
+            'name' => 'Red sus',
+            'color' => 'Red',
+            'role' => 2
+        ]);
+        Amongus::create([
+            'name' => 'Sussy baka',
+            'color' => 'Green',
+            'role' => 2
+        ]);
+        Amongus::create([
+            'name' => 'Orang',
+            'color' => 'Orange',
+            'role' => 1
+        ]);
+        Amongus::create([
+            'name' => 'Sussi baki',
+            'color' => 'Pink',
+            'role' => 1
+        ]);
     }
 
     /**
